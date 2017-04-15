15fb810720a2491d354b78826cc20dadd0e996bd
Armin Rigo <arigo@tunes.org>
2017-04-15 08:55:34 +0200

    Add controls

---
+++ Assets/Scripts/TrackZoomMotion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackZoomMotion : MonoBehaviour {
+
+    public Transform starScene;
+
+    SteamVR_TrackedController controller;
+    bool tracking;
+    Vector3 prev_position;
+
+	void Start()
+    {
+        controller = GetComponent<SteamVR_TrackedController>();
+        controller.TriggerClicked += Controller_TriggerClicked;
+        controller.TriggerUnclicked += Controller_TriggerUnclicked; ;
+    }
+
+    private void Controller_TriggerClicked(object sender, ClickedEventArgs e)
+    {
+        prev_position = transform.position;
+        tracking = true;
+    }
+
+    private void Controller_TriggerUnclicked(object sender, ClickedEventArgs e)
+    {
+        tracking = false;
+    }
+
+    void Update()
+    {
+		if (tracking)
+        {
+            Vector3 center = starScene.position;
+            Vector3 new_position = transform.position;
+            float factor = (new_position - center).magnitude / (prev_position - center).magnitude;
+            starScene.localScale *= factor;
+            prev_position = new_position;
+        }
+	}
+}
---
+++ Assets/TrackWheelMotion.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackWheelMotion : MonoBehaviour {
+
+    public Material star_material;
+
+    SteamVR_TrackedController controller;
+    bool tracking;
+    float prev_position;
+
+    void Start()
+    {
+        controller = GetComponent<SteamVR_TrackedController>();
+        controller.PadTouched += Controller_PadTouched;
+        controller.PadUntouched += Controller_PadUntouched;
+    }
+
+    private void Controller_PadTouched(object sender, ClickedEventArgs e)
+    {
+        prev_position = e.padY;
+        tracking = true;
+    }
+
+    private void Controller_PadUntouched(object sender, ClickedEventArgs e)
+    {
+        tracking = false;
+    }
+
+    void Update()
+    {
+        if (tracking)
+        {
+            var device = SteamVR_Controller.Input((int)controller.controllerIndex);
+            float new_position = device.GetAxis().y;
+            float delta = new_position - prev_position;
+            star_material.SetFloat("_LightFactor", star_material.GetFloat("_LightFactor") * Mathf.Pow(2f, delta));
+            prev_position = new_position;
+        }
+    }
+}
